18ff94c717d0675d8bee6219444467644184bee7
piyushsarkale23 <piyushsarkale35@gmail.com>
2023-04-05 18:24:05 +0530

    Add project files.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ReverseStringProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //reverse a string
+            //Console.WriteLine("enter a string value");
+            //string str = Console.ReadLine();
+            //Console.WriteLine(str);
+
+            //for( int i =str.Length;i>=0;i--)
+            //{
+            //    Console.WriteLine(str[i]);
+            ////}
+            ///
+
+            //Console.WriteLine("Enter a string: ");
+            //string inputString = Console.ReadLine();
+
+            //Console.WriteLine("Reversed string: ");
+            //for (int i = inputString.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(inputString[i]);
+            //}
+            //Console.WriteLine();
+
+            //palindrome
+            ////nitin = nitin
+            //Console.WriteLine("enter a string to check if it us palindrome or not");
+            //string str = Console.ReadLine();
+            //Console.WriteLine(str);
+
+            //for (int i = str.Length; i >= 0; i--)
+            //{
+
+            //    Console.Write(str[i]);
+            //}
+            //Console.WriteLine();
+
+            //if (str == str[i])
+            //{
+
+            //}
+
+            //Console.WriteLine("Enter a string: ");
+            //string inputString = Console.ReadLine();
+
+            //Console.WriteLine("Reversed string: ");
+            //for (int i = inputString.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(inputString[i]);
+            //}
+            //Console.WriteLine();
+            //if(inputString== inputString.)
+            //{
+            //    Console.WriteLine("Palindrome");
+            //}
+            //Console.WriteLine("Enter a string: ");
+            //string inputString = Console.ReadLine();
+            //int length = inputString.Length;
+            //bool isPalindrome = true;
+
+            //for (int i = 0; i < length / 2; i++)
+            //{
+            //    if (inputString[i] != inputString[length - i - 1])
+            //    {
+            //        isPalindrome = false;
+            //        break;
+            //    }
+            //}
+
+            //if (isPalindrome)
+            //{
+            //    Console.WriteLine("The string is a palindrome.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The string is not a palindrome.");
+            //}
+
+            //Console.WriteLine("enter first string");
+            //Console.WriteLine("enter second string");
+            //string str1 = Console.ReadLine();
+            //string str2 = Console.ReadLine();
+            //if (str1.Length == str2.Length)
+            //{
+
+            //    for (int i = str2.Length - 1; i >= 0; i--)
+            //    {
+            //       // Console.Write(str2[i]);
+            //    }
+            //     if (str1==str2)
+            //    {
+            //        Console.WriteLine("same");
+            //    }
+
+            //}
+
+
+
+
+            Console.WriteLine("Enter a string: ");
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine("Reversed string: ");
+            for (int i = inputString.Length - 1; i >= 0; i--)
+            {
+                Console.Write(inputString[i]);
+            }
+            Console.WriteLine();
+        }
+
+    }
+    }
+
+
